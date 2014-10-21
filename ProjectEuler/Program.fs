cfef691c7b9775ba7e7264958344bd51e52d45a0
rubenfa <ruben.fernandez.alfonso@outlook.com>
2014-10-21 11:21:11 +0200

    Problemas 1 y 2

---
+++ ProjectEuler/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Problem2
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" Problem2.StartProblem
+    let result = Console.ReadKey(true)
+
+    0 // return an integer exit code
+
+    
